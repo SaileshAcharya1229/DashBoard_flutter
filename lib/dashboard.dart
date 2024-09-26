ef9b3c50ea99067ae1c2f1689660e046272f310c
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-09-26 22:26:25 +0545

    adding photos by using array.

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -4,13 +4,23 @@ class Dashboard extends StatelessWidget {
   Dashboard({super.key});
   var height, width;
 
+  List imgData = [
+    "asset/images/mcqs.png",
+    "asset/images/quiz.png",
+    "asset/images/pastpapers.png",
+    "asset/images/pdf.png",
+    "asset/images/job.png",
+    "asset/images/about.png"
+  ];
+  List Titles = ["MCQS", "QUIZ", "Past Papers", "PDF", "JOBS", "About"];
+
   @override
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
-          color: Colors.indigo,
+          color: Color.fromARGB(255, 8, 151, 156),
           child: Column(
             children: [
               Container(
@@ -20,9 +30,9 @@ class Dashboard extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // const SizedBox(
-                    //   height: 20,
-                    // ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(
                         top: 30,
@@ -73,7 +83,7 @@ class Dashboard extends StatelessWidget {
                             height: 10,
                           ),
                           Text(
-                            "Last Update: 7 Aug 2023",
+                            "Last Update: 26 september 2024",
                             style: TextStyle(
                               fontSize: 16,
                               color: Colors.white54,
@@ -104,12 +114,32 @@ class Dashboard extends StatelessWidget {
                   ),
                   shrinkWrap: true,
                   physics: NeverScrollableScrollPhysics(),
-                  itemCount: 6,
+                  itemCount: imgData.length,
                   itemBuilder: (context, index) {
                     return InkWell(
                       onTap: () {},
                       child: Container(
-                        margin: EdgeInsets.all(8),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                spreadRadius: 1,
+                                blurRadius: 6,
+                              ),
+                            ]),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset(
+                              imgData[index],
+                              width: 100,
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
